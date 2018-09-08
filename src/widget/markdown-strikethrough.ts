a0eed0f76cf0d32d3789f219412afddcbd6816e8
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:31:23 +0700

    switched line through text to widget creator

--- src/widget/markdown-strikethrough.ts
+++ src/widget/markdown-strikethrough.ts
@@ -1,33 +1,21 @@
 import { Editor } from "codemirror";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownStrikethrough(editor: Editor, line): void {
   const strikethroughRegex = /~~(.*?)~~/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(strikethroughRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = strikethroughRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: strikethroughRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 2
+    },
+    (match): HTMLElement => {
       const span = document.createElement("span");
       span.className = `cm-strikethrough`;
       span.innerText = match[1];
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
