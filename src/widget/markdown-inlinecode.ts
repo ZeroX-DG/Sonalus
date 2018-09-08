9a54661aabe55c9382b0dcb7f695351ab9289c32
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:24:01 +0700

    switched inline code to widget creator

--- src/widget/markdown-inlinecode.ts
+++ src/widget/markdown-inlinecode.ts
@@ -1,35 +1,21 @@
 import { Editor } from "codemirror";
-import { allowBreakMark } from "./utils";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownInlineCode(editor: Editor, line): void {
   const codeRegex = /(?<!\`)`(?!\`)(.+?)(?<!\`)`(?!\`)/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(codeRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = codeRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: codeRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 1
+    },
+    (match): HTMLElement => {
       const span = document.createElement("span");
       span.className = `inline-code`;
       span.innerText = match[1];
-      allowBreakMark(editor, lineNo, range, span, 1);
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
