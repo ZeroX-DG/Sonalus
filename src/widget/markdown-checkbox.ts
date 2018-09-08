800a0bfd9fec5b14a45b4b79dba45c01b2de9d36
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 23:27:55 +0700

    improved widget creator

--- src/widget/markdown-checkbox.ts
+++ src/widget/markdown-checkbox.ts
@@ -1,44 +1,35 @@
 import { Editor } from "codemirror";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownCheckbox(editor: Editor, line): void {
   const checkboxRegex = /\-\s\[(x| )?\]\s/;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(checkboxRegex)) {
-    return;
-  }
-  let match = checkboxRegex.exec(line.text);
-  if (
-    match &&
-    (cursor.ch < match.index ||
-      cursor.ch > match.index + match[0].length ||
-      cursor.line !== lineNo)
-  ) {
-    const range = {
-      from: match.index,
-      to: match.index + match[0].length
-    };
-    const checkbox = document.createElement("input");
-    checkbox.className = "markdown-checkbox";
-    checkbox.type = "checkbox";
-    checkbox.onclick = () => {
-      const nextState = checkbox.checked ? "x" : " ";
-      doc.replaceRange(
-        nextState,
-        { line: lineNo, ch: range.from + 3 },
-        { line: lineNo, ch: range.from + 4 }
-      );
-    };
-    if (match[1] === "x") {
-      checkbox.checked = true;
-    } else {
-      checkbox.checked = false;
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: checkboxRegex,
+      matchMany: false,
+      allowBreak: false
+    },
+    (match, range): HTMLElement => {
+      const checkbox = document.createElement("input");
+      const doc = editor.getDoc();
+      const lineNo = line.lineNo();
+      checkbox.className = "markdown-checkbox";
+      checkbox.type = "checkbox";
+      checkbox.onclick = () => {
+        const nextState = checkbox.checked ? "x" : " ";
+        doc.replaceRange(
+          nextState,
+          { line: lineNo, ch: range.from + 3 },
+          { line: lineNo, ch: range.from + 4 }
+        );
+      };
+      if (match[1] === "x") {
+        checkbox.checked = true;
+      } else {
+        checkbox.checked = false;
+      }
+      return checkbox;
     }
-    doc.markText(
-      { line: lineNo, ch: range.from },
-      { line: lineNo, ch: range.to },
-      { replacedWith: checkbox, clearOnEnter: true }
-    );
-  }
+  );
 }
--- src/widget/widget.ts
+++ src/widget/widget.ts
@@ -25,10 +25,53 @@ export class WidgetCreator {
   ) {
     if (matchMany) {
       this.createMatchManyWidget(regex, allowBreak, breakOffset, createElement);
+    } else {
+      this.createMatchSingleWidget(
+        regex,
+        allowBreak,
+        breakOffset,
+        createElement
+      );
     }
   }
 
-  createMatchManyWidget(regex, allowBreak, breakOffset, createElement) {
+  private createMatchSingleWidget(
+    regex,
+    allowBreak,
+    breakOffset,
+    createElement
+  ) {
+    const doc = this.editor.getDoc();
+    const cursor = doc.getCursor();
+    const line = this.line;
+    const lineNo = line.lineNo();
+    if (!line.text.match(regex)) {
+      return;
+    }
+    let match = regex.exec(line.text);
+    if (
+      match &&
+      (cursor.ch < match.index ||
+        cursor.ch > match.index + match[0].length ||
+        cursor.line !== lineNo)
+    ) {
+      const range = {
+        from: match.index,
+        to: match.index + match[0].length
+      };
+      const element: HTMLElement = createElement(match, range);
+      if (allowBreak) {
+        allowBreakMark(this.editor, lineNo, range, element, breakOffset);
+      }
+      doc.markText(
+        { line: lineNo, ch: range.from },
+        { line: lineNo, ch: range.to },
+        { replacedWith: element, clearOnEnter: true }
+      );
+    }
+  }
+
+  private createMatchManyWidget(regex, allowBreak, breakOffset, createElement) {
     const doc = this.editor.getDoc();
     const cursor = doc.getCursor();
     const line = this.line;
@@ -48,7 +91,7 @@ export class WidgetCreator {
           from: match.index,
           to: match.index + match[0].length
         };
-        const element: HTMLElement = createElement(match);
+        const element: HTMLElement = createElement(match, range);
         if (allowBreak) {
           allowBreakMark(this.editor, lineNo, range, element, breakOffset);
         }
