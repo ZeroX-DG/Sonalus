7d77ac4324b952373476213124bfca7645a5642f
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:15:40 +0700

    switched highlight text to widget creator

--- src/widget/markdown-highlight.ts
+++ src/widget/markdown-highlight.ts
@@ -1,35 +1,21 @@
 import { Editor } from "codemirror";
-import { allowBreakMark } from "./utils";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownHighlight(editor: Editor, line): void {
   const highlightRegex = /\<mark\>(.+?)\<\/mark\>/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(highlightRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = highlightRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: highlightRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 6
+    },
+    (match): HTMLElement => {
       const span = document.createElement("span");
       span.className = `cm-mark`;
       span.innerText = match[1];
-      allowBreakMark(editor, lineNo, range, span, 6);
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
