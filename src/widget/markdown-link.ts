c13cc45b25ff19f4142c0066ecaf55ae3fd1a16d
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:28:45 +0700

    switched link to widget creator

--- src/widget/markdown-link.ts
+++ src/widget/markdown-link.ts
@@ -1,34 +1,25 @@
 import { Editor } from "codemirror";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownLink(editor: Editor, line): void {
-  const linkRegex = /\[(.*?)\]\((.*?)\)/;
-  const match = line.text.match(linkRegex);
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (
-    match &&
-    line.text[match.index - 1] !== "!" &&
-    (cursor.ch < match.index ||
-      cursor.ch > match.index + match[0].length ||
-      cursor.line !== lineNo)
-  ) {
-    const range = {
-      from: match.index,
-      to: match.index + match[0].length
-    };
-    const link = document.createElement("a");
-    const span = document.createElement("span");
-    span.className = "link-icon";
-    span.innerText = "🔗";
-    link.appendChild(span);
-    link.appendChild(document.createTextNode(match[1]));
-    link.href = match[2];
-    link.className = "link-button";
-    doc.markText(
-      { line: lineNo, ch: range.from },
-      { line: lineNo, ch: range.to },
-      { replacedWith: link, clearOnEnter: true }
-    );
-  }
+  const linkRegex = /(?<!\!)\[(.*?)\]\((.*?)\)/g;
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: linkRegex,
+      matchMany: true,
+      allowBreak: false
+    },
+    (match): HTMLElement => {
+      const link = document.createElement("a");
+      const span = document.createElement("span");
+      span.className = "link-icon";
+      span.innerText = "🔗";
+      link.appendChild(span);
+      link.appendChild(document.createTextNode(match[1]));
+      link.href = match[2];
+      link.className = "link-button";
+      return link;
+    }
+  );
 }
