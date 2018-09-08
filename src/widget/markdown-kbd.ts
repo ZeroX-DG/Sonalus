951ba692fb45e6c6ee1e170931eaedcba61867e6
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:00:01 +0700

    switched kdb to widget creator

--- src/widget/markdown-kbd.ts
+++ src/widget/markdown-kbd.ts
@@ -1,34 +1,20 @@
 import { Editor } from "codemirror";
-import { allowBreakMark } from "./utils";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownKBD(editor: Editor, line): void {
   const kbdRegex = /\<kbd\>(\w+?)\<\/kbd\>/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(kbdRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = kbdRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: kbdRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 5
+    },
+    (match): HTMLElement => {
       const span = document.createElement("kbd");
       span.innerText = match[1] || match[2];
-      allowBreakMark(editor, lineNo, range, span, 5);
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
