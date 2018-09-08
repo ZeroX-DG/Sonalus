b49fd16055de63f2ef75eed72d15f0ed8d0a6a9d
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:07:07 +0700

    switched bold text to widget creator

--- src/widget/markdown-bold.ts
+++ src/widget/markdown-bold.ts
@@ -1,35 +1,21 @@
 import { Editor } from "codemirror";
-import { allowBreakMark } from "./utils";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownBold(editor: Editor, line): void {
   const boldRegex = /(?<!\*)\*\*(?!\*)(.+?)(?<!\*)\*\*(?!\*)|(?<!\_)\_\_(?!\_)(.+?)(?<!\_)\_\_(?!\_)/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(boldRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = boldRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: boldRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 2
+    },
+    (match): HTMLElement => {
       const span = document.createElement("span");
       span.className = `cm-strong`;
-      allowBreakMark(editor, lineNo, range, span, 2);
       span.innerText = match[1] || match[2];
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
