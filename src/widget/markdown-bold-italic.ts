5e495bc4a5960c859179ed154fe6a413bace0746
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-08 15:05:08 +0700

    switched bold italic text to widget creator

--- src/widget/markdown-bold-italic.ts
+++ src/widget/markdown-bold-italic.ts
@@ -1,35 +1,21 @@
 import { Editor } from "codemirror";
-import { allowBreakMark } from "./utils";
+import { WidgetCreator } from "./widget";
 
 export function MarkdownBoldItalic(editor: Editor, line): void {
   const boldItalicRegex = /(?<!\*)\*\*\*(?!\*)(.+?)(?<!\*)\*\*\*(?!\*)/g;
-  const doc = editor.getDoc();
-  const cursor = doc.getCursor();
-  const lineNo = line.lineNo();
-  if (!line.text.match(boldItalicRegex)) {
-    return;
-  }
-  let match = null;
-  while ((match = boldItalicRegex.exec(line.text))) {
-    if (
-      match &&
-      (cursor.ch < match.index ||
-        cursor.ch > match.index + match[0].length ||
-        cursor.line !== lineNo)
-    ) {
-      const range = {
-        from: match.index,
-        to: match.index + match[0].length
-      };
+  const widgetCreator = new WidgetCreator(editor, line);
+  widgetCreator.createWidget(
+    {
+      regex: boldItalicRegex,
+      matchMany: true,
+      allowBreak: true,
+      breakOffset: 3
+    },
+    (match): HTMLElement => {
       const span = document.createElement("span");
       span.className = `cm-strong cm-em`;
       span.innerText = match[1];
-      allowBreakMark(editor, lineNo, range, span, 3);
-      doc.markText(
-        { line: lineNo, ch: range.from },
-        { line: lineNo, ch: range.to },
-        { replacedWith: span, clearOnEnter: true }
-      );
+      return span;
     }
-  }
+  );
 }
